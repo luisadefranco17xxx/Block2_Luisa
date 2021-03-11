878e9d59813491f0451ba998af5e1b810c71e100
Luisa <massimo.e.luisa.au@gmail.com>
2021-03-11 08:18:51 +0100

    buzzfrizz verschlecterung

--- src/FizzBuzzApp.java
+++ src/FizzBuzzApp.java
@@ -2,14 +2,14 @@ public class FizzBuzzApp {
     public static void main(String[] args) {
 
         int[] arr = {
-
+               3,5,15,
                98, 97, 87, 86, 3, 81, 77, 74, 72, 69, 68, 65, 61,
                 51, 52, 59, 54, 47, 111, 42, 38, 51, 37, 29, 24, 81,
                 12, 29, 2, 17, 8   };
 
         for (int i=0; i<arr.length; i++) {
             writeFizzBuzz(arr[i]);
-            System.out.println("Number "+arr[i]+ "the divisors are" +writeFizzBuzz(arr[i]));
+            System.out.println("Number "+arr[i]+ ", the divisors are " +writeFizzBuzz(arr[i]));
 
 
         }
@@ -19,18 +19,22 @@ public class FizzBuzzApp {
     public static String writeFizzBuzz(int value){
         String  stringToPrint;
         stringToPrint = " ";
+
         System.out.println();
         if (value%3==0){
                 //stringToPrint.concat("Frizz");
+            System.out.print("Frizz");
             stringToPrint="Frizz";
-            System.out.print(stringToPrint);
+            //System.out.print(stringToPrint);
         }
         if (value%5==0){
            //stringToPrint.concat("Buzz");
-            //stringToPrint="Buzz";
-            //System.out.print("soll String.concat="+stringToPrint);
+            stringToPrint=stringToPrint+"Buzz";
+
             System.out.print("Buzz");
-        } else  {
+        } else if (!(value%3==0)) {
+            stringToPrint=" ";
+            System.out.println(value);
             //return value.toString();
             //soll value zurück geben
         }
