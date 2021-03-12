fa81dd91edc6ff63325373257a90dae042fa54d5
Luisa <massimo.e.luisa.au@gmail.com>
2021-03-12 12:54:04 +0100

    buzzfrizz finished

--- src/FizzBuzzApp.java
+++ src/FizzBuzzApp.java
@@ -1,15 +1,23 @@
+import java.util.Arrays;
+
 public class FizzBuzzApp {
     public static void main(String[] args) {
 
         int[] arr = {
-               3,5,15, 34 };
-               /*98, 97, 87, 86, 3, 81, 77, 74, 72, 69, 68, 65, 61,
+                3, 5, 15, 34,
+                98, 97, 87, 86, 3, 81, 77, 74, 72, 69, 68, 65, 61,
                 51, 52, 59, 54, 47, 111, 42, 38, 51, 37, 29, 24, 81,
-                12, 29, 2, 17, 8   };*/
+                12, 29, 2, 17, 8   };
 
         for (int i=0; i<arr.length; i++) {
             System.out.println(writeFizzBuzz(arr[i]));
+            System.out.println();
         }
+        String[] my_resultString = new String[100];
+        my_resultString=saveResult();   //the method saveResult saves the result in a string:my_resultString
+
+        printMyResultString(my_resultString);
+
     }
 
 
@@ -17,7 +25,6 @@ public class FizzBuzzApp {
         String  stringToPrint;
         stringToPrint = " ";
 
-        System.out.println();
         if (value%3==0){
             stringToPrint="Frizz";
         }
@@ -29,5 +36,23 @@ public class FizzBuzzApp {
         }
 
         return stringToPrint;
+    }
+
+    public static String[] saveResult() {      //This methods saves the result of wirteFizzBuss in a string
+        String[] resultArray = new String[100];
+        for (int i=0; i<100;i++) {
+            resultArray[i]= writeFizzBuzz(i);
+        }
+        return resultArray;
+    }
+
+    public static void printMyResultString(String[] my_string) {     //This method prints the result for 1 bis 100 in the screen
+
+        for (int i = 0; i < 100; i++) {
+            System.out.print(my_string[i]+", ");
+        }
+        System.out.println();
+        System.out.println("second way to print:");
+        System.out.println(Arrays.deepToString(my_string));
     }
 }
